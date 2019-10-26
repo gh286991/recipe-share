7e5a31b0d49fc0a034a0388cbc1d573adec74d1e
TomJhuang <gh286991@gmail.com>
2019-10-26 23:37:30 +0800

    user comfirm

--- recipes/models.py
+++ recipes/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
-    image_path = models.CharField(max_length=100)
+    image_path = models.CharField(max_length=600)
     description = models.TextField()
     author = models.CharField(max_length=100)
     Test = models.CharField(default="Test", max_length=100)
